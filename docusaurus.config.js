42bb5befacaf98d52b642c73d4c0b25e5d45d632
techwriters-aurigo <85871451+techwriters-aurigo@users.noreply.github.com>
2021-08-18 16:27:21 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,7 +54,7 @@ module.exports = {
       },
       items: [
        {
-          href: 'javascript:window.print()',
+          href: '#',
           className: 'forprint',
           // Only one of "to" or "href" should be used
           // href: 'https://www.facebook.com',
